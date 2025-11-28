7ae80ca916f5d4757d7946cd6c708c9ebe4cd948
samuelacpessoa <samuelacpessoa@gmail.com>
2025-11-28 17:35:58 -0300

    Update GuiController.java

--- ProjetoFinalPOO/src/projetofinal/gui/GuiController.java
+++ ProjetoFinalPOO/src/projetofinal/gui/GuiController.java
@@ -4,7 +4,7 @@ import projetofinal.gui.telas.TelaPrincipal;
 public class GuiController {
     private static BancoService bancoService;
 
-    // Inicializa o banco apenas uma vez
+    
     public static BancoService getBancoService() {
         if (bancoService == null) {
             bancoService = new BancoService("Banco POO");
@@ -12,7 +12,7 @@ public class GuiController {
         return bancoService;
     }
 
-    // Método para abrir a tela principal ao iniciar
+    
     public static void iniciar() {
         new TelaPrincipal().setVisible(true);
     }   
